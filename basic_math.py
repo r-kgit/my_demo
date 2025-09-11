b925a5d52687f0945e3efea16f3e877b499e982e
r-kgit <kumarirekha12346@gmail.com>
2025-09-11 15:00:01 +0530

    updatd 11/09/25

--- basic_math.py
+++ basic_math.py
@@ -1,13 +1,16 @@
 # basic math calculation  in python
-num1=int(input("Enter the first no : "))
-num2=int(input("Enter the second number : "))
-sum=num1+num2
-sub=num1-num2
-mult=num1*num2
-div=num1/num2
-print("Addition :", sum)
+from pip._internal.vcs import git
+
+num1 = int(input("Enter the first no : "))
+num2 = int(input("Enter the second number : "))
+add = num1+num2
+sub = num1-num2
+mult = num1*num2
+div = num1/num2
+print("Addition :", add)
 print("Subtraction :", sub)
 print("Addition :", mult)
 print("Addition :", div)
+
 
 
