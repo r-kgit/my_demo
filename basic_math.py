3a31131999f2dc41bd073c217e7a514a8becc8cd
r-kgit <kumarirekha12346@gmail.com>
2025-09-11 13:11:01 +0530

    basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,13 @@
+# basic math calculation  in python
+num1=int(input("Enter the first no : "))
+num2=int(input("Enter the second number : "))
+sum=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+print("Addition :", sum)
+print("Subtraction :", sub)
+print("Addition :", mult)
+print("Addition :", div)
+
+
